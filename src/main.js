abd4774f25ed4c415715869c1e2787fd62fc4d11
hong <sunghoo78@ipartners.co.kr>
2022-09-29 16:13:47 +0900

    서니 애니메이션

--- src/main.js
+++ src/main.js
@@ -5,8 +5,15 @@ const Main = () => {
     return(
         <div>
             <div className='wrap'>
+                {/* 써니 */}
+                <div className='charactor-sunny__wrap'>
+                    <div className='charactor-sunny__wrap--inbody'>
+                        <div className='eya-wrap'></div>
+                        <div className='lib-wrap'></div>
 
-
+                    </div>
+                </div>
+                {/* //써니 */}
             </div>
         </div>
     )
